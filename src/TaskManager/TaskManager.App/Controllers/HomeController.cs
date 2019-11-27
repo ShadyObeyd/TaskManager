f7e493f282a1b27b2153855eff50eee61cf81a98
ShadyObeyd <obeyd.shady@gmail.com>
2019-11-27 10:14:16 +0200

    Created default ASP.NET Core project.

---
+++ src/TaskManager/TaskManager.App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskManager.App.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using TaskManager.App.Models;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
