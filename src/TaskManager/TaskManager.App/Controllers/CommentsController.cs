3524354afc5400497f9a0e6e4cd4d02713bf6aa0
ShadyObeyd <obeyd.shady@gmail.com>
2019-11-28 13:47:17 +0200

    Users can now add comments.

---
+++ src/TaskManager/TaskManager.App/Controllers/CommentsController.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskManager.App.Controllers
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Services;
+    using TaskManager.Models.DataModels;
+
+    public class CommentsController : Controller
+    {
+        private readonly CommentService commentService;
+        private readonly UserManager<TaskManagerUser> userManager;
+        public CommentsController(CommentService commentService, UserManager<TaskManagerUser> userManager)
+        {
+            this.commentService = commentService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public IActionResult Create(string taskId, string content)
+        {
+            if (string.IsNullOrEmpty(taskId) || string.IsNullOrEmpty(content))
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            var userId = this.userManager.GetUserId(HttpContext.User);
+            this.commentService.CreateComment(taskId, content, userId);
+
+            return this.RedirectToAction("Open", "Tasks", new { taskId });
+        }
+    }
+}
--- src/TaskManager/TaskManager.Services/CommentService.cs
+++ src/TaskManager/TaskManager.Services/CommentService.cs
@@ -5,6 +5,8 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
+    using Models.DataModels;
+    using System;
 
     public class CommentService
     {
@@ -23,6 +25,20 @@
                 Creator = c.Creator.UserName,
                 DateAdded = c.DateAdded.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture)
             }).ToList();
+        }
+        
+        public void CreateComment(string taskId, string content, string creatorId)
+        {
+            var comment = new Comment
+            {
+                TaskId = taskId,
+                Content = content,
+                DateAdded = DateTime.Now,
+                CreatorId = creatorId
+            };
+
+            this.db.Comments.Add(comment);
+            this.db.SaveChanges();
         }
     }
 }
